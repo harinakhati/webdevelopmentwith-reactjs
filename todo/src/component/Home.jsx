729841b5f2b10a8040628ed4463c1bf9bb9f61e8
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-08-13 15:49:24 +0545

    todo/src/component/Home.jsx todo/src/component/Home.module.css todo/src/component/Home.modules.css

--- todo/src/component/Home.jsx
+++ todo/src/component/Home.jsx
@@ -1,10 +1,10 @@
-import "./Home.modules.css"
-// import css from "./Home.modules.css"
+// import "./Home.module.css"
+import css from "./Home.module.css"
 const Home=()=>{
     return( 
         <div>
-        <h3 className="title">Home Title</h3> 
-        <p className="text" style={{color:"black"}}>Hello, This is Home Page.</p>;
+        <h3 className={css.title}>Home Title</h3> 
+        <p className={css.text} style={{color:"black"}}>Hello, This is Home Page.</p>;
         </div>
     );
 };
