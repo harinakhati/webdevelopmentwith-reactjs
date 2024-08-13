6595544e07825b8fb621714b62a44a32f239b830
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-08-13 13:03:02 +0545

    todo/src/component/TextBox.jsx

--- todo/src/component/TextBox.jsx
+++ todo/src/component/TextBox.jsx
@@ -1,11 +1,19 @@
+import {useState} from "react";
 const TextBox=()=>{
+    const[name,setName]=useState('')
+
     const onPClick=(v)=>{
         alert("Button clicked");
     }
+    const updateName=(e)=>{
+        const username=e.target.value;
+    setName(username);
+    }
     return(
         <div>
-            <p onClick={onPClick}>This is me.</p>
+            <p onClick={onPClick}>This is me.{name}</p>
+            <input value={name} onChange={updateName} />
         </div>
     );
 }
-export default TextBox
+export default TextBox;
