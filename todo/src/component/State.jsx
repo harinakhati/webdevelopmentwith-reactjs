9e34daa95105ab4141266923dbfa89b69e106ada
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-08-13 15:31:28 +0545

    todo/src/component/State.jsx

--- todo/src/component/State.jsx
+++ todo/src/component/State.jsx
@@ -6,7 +6,7 @@ function State(){
     return (
         <div className="name">
         <p>Count: {count}</p>
-        <button onClick={()=> setCount(count+1)}>Incerement</button>
+        <button onClick={()=> setCount(count+1)}>Increment</button>
         <button onClick={()=> setCount(count-1)}>Decrement</button>
         <button onClick={()=>setCount(0)}>Reset
         </button>
