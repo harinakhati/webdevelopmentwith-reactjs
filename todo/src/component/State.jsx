154de70beb0b8a7db89984320b5b8bb8bc9c907c
Harina Khati <155285445+harinakhati@users.noreply.github.com>
2024-08-13 12:25:47 +0545

    todo/src/component/State.jsx

--- todo/src/component/State.jsx
+++ todo/src/component/State.jsx
@@ -2,12 +2,21 @@ import React, {useState} from "react";
 
 function State(){
     const[count, setCount] = useState(0);
+    const[name, setName] =useState(0);
     return (
-        <div>
+        <div className="name">
         <p>Count: {count}</p>
-        <button onClick={()=> setCount(count+1)}>Increment</button>
+        <button onClick={()=> setCount(count+1)}>Incerement</button>
         <button onClick={()=> setCount(count-1)}>Decrement</button>
-        <button onClick={()=>setCount(0)}>Reset</button>
+        <button onClick={()=>setCount(0)}>Reset
+        </button>
+        <br />
+        <p>Name:{name}</p>
+        <button onClick={()=>setName('Deadpool')}>
+            Set name as Moviecharacter 
+        </button>
+        <button onClick={()=>setName('')}>reset name</button>
+        <button onClick={()=>setName('wolfrine')}>Set name as Anime </button>
         </div>
     );
 }
